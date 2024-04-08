9e508ce182a9945bfa74675ab7b7e04cd4922aae
david-parkk <ji0513ji@naver.com>
2024-04-08 15:34:05 +0900

    feat RedisUsrController 추가

---
+++ src/main/java/kutaverse/game/map/controller/RedisUserController.java
@@ -0,0 +1,39 @@
+package kutaverse.game.map.controller;
+
+import kutaverse.game.map.domain.User;
+import kutaverse.game.map.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Controller
+@RequiredArgsConstructor
+public class RedisUserController implements UserController{
+
+    private final UserService userService;
+
+    @PostMapping("/user")
+    @Override
+    public Mono<Boolean> addUser(@RequestBody User user) {
+        return userService.create(user);
+    }
+
+    @GetMapping("/user")
+    @Override
+    public Flux<User> getAllUser() {
+        return userService.getAll();
+    }
+
+    @GetMapping("/user/{key}")
+    @Override
+    public Mono<User> getUser(@PathVariable String key) {
+        return userService.getOne(key).cast(User.class);
+    }
+
+    @DeleteMapping("/user/{key}")
+    public Mono<Long> deleteDessert(@PathVariable String key) {
+        return userService.deleteById(key);
+    }
+}
