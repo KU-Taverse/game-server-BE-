435456457f1cb655fae3a4156fc271a7cac26b49
david-parkk <ji0513ji@naver.com>
2024-04-15 01:00:24 +0900

    feat : websocket custom handler

---
+++ src/main/java/kutaverse/game/map/websocket/handler/ResponseHandler.java
@@ -0,0 +1,8 @@
+package kutaverse.game.map.websocket.handler;
+
+public class ResponseHandler implements WebSocketHandler {
+    @Override
+    public String handle(String e) {
+        return null;
+    }
+}
---
+++ src/main/java/kutaverse/game/map/websocket/handler/SaveHandler.java
@@ -0,0 +1,29 @@
+package kutaverse.game.map.websocket.handler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kutaverse.game.map.dto.UserRequestDto;
+import kutaverse.game.map.service.UserService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class SaveHandler implements WebSocketHandler {
+
+    private final UserService userService;
+    @Override
+    public String handle(String e) {
+        try{
+            ObjectMapper objectMapper=new ObjectMapper();
+            UserRequestDto mapRequestDto=objectMapper.readValue(e, UserRequestDto.class);
+
+        } catch (
+                JsonMappingException ex) {
+            throw new RuntimeException("jsonMappring Exception 발생");
+        } catch (
+                JsonProcessingException ex) {
+            throw new RuntimeException("jsonprocessingexception 발생");
+        }
+        return null;
+    }
+}
