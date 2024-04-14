e08bb00ba27098339ac050cb4ec5e5ebf638ea34
david-parkk <ji0513ji@naver.com>
2024-04-15 01:01:36 +0900

    refactor : handler 분리

--- src/main/java/kutaverse/game/map/websocket/CustomWebSocketHandler.java
+++ src/main/java/kutaverse/game/map/websocket/CustomWebSocketHandler.java
@@ -1,13 +1,9 @@
 package kutaverse.game.map.websocket;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import kutaverse.game.map.dto.MapRequestDto;
+import kutaverse.game.map.websocket.handler.WebSocketHandler;
+import kutaverse.game.map.websocket.handler.WebSocketHandlerMapping;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Sinks;
@@ -15,7 +11,7 @@ import reactor.core.publisher.Sinks;
 
 @Component
 @Slf4j
-public class CustomWebSocketHandler implements WebSocketHandler {
+public class CustomWebSocketHandler implements org.springframework.web.reactive.socket.WebSocketHandler {
 
 	private final Sinks.Many<String> sink;
 
@@ -32,16 +28,9 @@ public class CustomWebSocketHandler implements WebSocketHandler {
 				// 메시지를 JSON 객체로 변환
 				.map(e -> e.getPayloadAsText())
 				.map(e -> {
-					try {
-						// 메시지를 파싱
-						ObjectMapper objectMapper=new ObjectMapper();
-						MapRequestDto mapRequestDto=objectMapper.readValue(e,MapRequestDto.class);
-						return mapRequestDto.toString();
-					} catch (JsonMappingException ex) {
-						throw new RuntimeException("jsonMappring Exception 발생");
-					} catch (JsonProcessingException ex) {
-						throw new RuntimeException("jsonprocessingexception 발생");
-					}
+					WebSocketHandler webSocketHandler= WebSocketHandlerMapping.getHandler(e);
+
+					return webSocketHandler.handle(e);
 				});
 
 		output.subscribe(s -> sink.emitNext(s, Sinks.EmitFailureHandler.FAIL_FAST));
