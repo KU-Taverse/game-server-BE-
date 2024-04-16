3250a563d465f07ff9511a79fb7eb66b267574c6
david-parkk <ji0513ji@naver.com>
2024-04-17 01:35:05 +0900

    feat : sendMessageToClients 변경

--- src/main/java/kutaverse/game/map/websocket/MessageSender.java
+++ src/main/java/kutaverse/game/map/websocket/MessageSender.java
@@ -1,20 +1,42 @@
 package kutaverse.game.map.websocket;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import kutaverse.game.map.domain.User;
+import kutaverse.game.map.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.reactivestreams.Publisher;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.function.Function;
 
 @Component
+@RequiredArgsConstructor
 public class MessageSender {
 
     private final CustomWebSocketHandler webSocketHandler;
 
-    public MessageSender(CustomWebSocketHandler webSocketHandler) {
-        this.webSocketHandler = webSocketHandler;
-    }
+    private final UserService userService;
+
+    ObjectMapper objectMapper=new ObjectMapper();
 
     @Scheduled(fixedRate = 5000) // 5초마다 실행
     public void sendMessageToClients() {
-        String message = "\\{\"userId\":\"감자\",\"positionX\":\"1\",\"positionY\":\"2\",\"positionZ\":\"3\",\"eulerAngleX\":\"4\",\"eulerAngleY\":\"5\",\"eulerAngleZ\":\"6\",\"status\":\"7\"\\}";
-        webSocketHandler.sendMessageToAllClients(message);
+
+        userService.findAll().subscribe(u->sendUsersAsJsonToClients(u));
+    }
+
+    private Mono<Void> sendUsersAsJsonToClients(User user) {
+        try {
+            String json = objectMapper.writeValueAsString(user);
+            return webSocketHandler.sendMessageToAllClients(json);
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
