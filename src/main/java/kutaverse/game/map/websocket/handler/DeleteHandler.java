44b6fdae47ad16fa9d70a4e3e9fad4f992139032
david-parkk <ji0513ji@naver.com>
2024-05-04 01:38:42 +0900

    feat : deletehandler

---
+++ src/main/java/kutaverse/game/map/websocket/handler/DeleteHandler.java
@@ -0,0 +1,17 @@
+package kutaverse.game.map.websocket.handler;
+
+import kutaverse.game.map.dto.UserRequestDto;
+import kutaverse.game.map.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class DeleteHandler implements WebSocketHandler {
+
+    private final UserService userService;
+    @Override
+    public void handle(UserRequestDto userRequestDto) {
+        userService.deleteById(userRequestDto.getUserId()).subscribe();
+    }
+}
