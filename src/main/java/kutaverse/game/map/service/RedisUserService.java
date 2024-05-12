e5e3c7e832631c35bb16a5501c540be433f219b1
david-parkk <ji0513ji@naver.com>
2024-05-12 22:56:00 +0900

    fix : 0에서 NOTUSE 제외하도록 변경

--- src/main/java/kutaverse/game/map/service/RedisUserService.java
+++ src/main/java/kutaverse/game/map/service/RedisUserService.java
@@ -32,7 +32,7 @@ public class RedisUserService implements UserService {
     @Override
     public Flux<User> findAllByTime(long length){
         if(length==0)
-            return findAll();
+            return findAll().filter(user -> user.getStatus()!=Status.NOTUSE);
 
         return findAll().filter(user -> Duration.between(user.getLocalDateTime(), LocalDateTime.now()).toSeconds() < length && user.getStatus()!=Status.NOTUSE);
 
