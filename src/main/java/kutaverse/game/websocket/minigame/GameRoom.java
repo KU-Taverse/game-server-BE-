4a829a12cf212cebe52758381a5b3f23a8b072e3
kamothi <rhkddlf7911@naver.com>
2024-05-28 19:31:07 +0900

    Feat: 미니 게임 룸 생성

---
+++ src/main/java/kutaverse/game/websocket/minigame/GameRoom.java
@@ -0,0 +1,33 @@
+package kutaverse.game.websocket.minigame;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class GameRoom {
+    private final String roomId;
+
+    private final Map<String, WebSocketSession> players = new ConcurrentHashMap<>();
+
+    public GameRoom(String roomId) {
+        this.roomId = roomId;
+    }
+
+    public void addPlayer(String userId, WebSocketSession webSocketSession){
+        players.put(userId,webSocketSession);
+    }
+
+    public void removePlayer(String userId){
+        players.remove(userId);
+    }
+
+    // 이건 나중에 필요한 값들을 보내는 용도
+    public void sendMessage(String message) {
+        players.values().forEach(s ->{
+            s.send(Mono.just(s.textMessage(message))).subscribe();
+        });
+    }
+}
