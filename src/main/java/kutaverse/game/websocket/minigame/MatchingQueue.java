a134180b4e99918073e2092034d6bc1ff4a1ac6e
kamothi <rhkddlf7911@naver.com>
2024-05-28 19:20:24 +0900

    Feat: 매칭 큐 구현

---
+++ src/main/java/kutaverse/game/websocket/minigame/MatchingQueue.java
@@ -0,0 +1,31 @@
+package kutaverse.game.websocket.minigame;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketSession;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+// Queue를 통해 플레이어들을 저장해 둘 예정
+
+
+@Component
+public class MatchingQueue {
+    private static final Queue<Map.Entry<String, WebSocketSession>> queueing = new ConcurrentLinkedQueue<>();
+
+    public static void addPlayer(String userId, WebSocketSession webSocketSession){
+        queueing.offer(new AbstractMap.SimpleEntry<>(userId,webSocketSession));
+    }
+
+    public static void getPlayer(String userId){
+        queueing.poll();
+    }
+
+    public static int queueingSize(){
+        return queueing.size();
+    }
+
+
+}
