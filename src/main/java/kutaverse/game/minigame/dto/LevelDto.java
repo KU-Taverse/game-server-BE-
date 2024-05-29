51374fe728c069edf3082a65304b60b6dbd9c205
kamothi <rhkddlf7911@naver.com>
2024-05-29 11:20:07 +0900

    feat: toString 어노테이션 추가

--- src/main/java/kutaverse/game/minigame/dto/LevelDto.java
+++ src/main/java/kutaverse/game/minigame/dto/LevelDto.java
@@ -1,16 +1,21 @@
 package kutaverse.game.minigame.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
+@ToString
 public class LevelDto {
     private int level;
     private List<PositionDto> positions;
 
     public LevelDto() {
     }
+
 
     public LevelDto(int level, List<PositionDto> positions) {
         this.level = level;
--- src/main/java/kutaverse/game/minigame/dto/PositionDto.java
+++ src/main/java/kutaverse/game/minigame/dto/PositionDto.java
@@ -1,8 +1,12 @@
 package kutaverse.game.minigame.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class PositionDto {
     private int x;
     private int z;
@@ -11,7 +15,7 @@ public class PositionDto {
     public PositionDto() {
     }
 
-    public PositionDto(int x, int z, int rotation) {
+    public PositionDto(int x, int z,int rotation) {
         this.x = x;
         this.z = z;
         this.rotation = rotation;
