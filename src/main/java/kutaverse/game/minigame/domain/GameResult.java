1c21d33acc4e9c1980b8830f8ade15b5ae7387df
kamothi <rhkddlf7911@naver.com>
2024-06-03 22:07:20 +0900

    Feat: 게임결과 엔티티 생성

---
+++ src/main/java/kutaverse/game/minigame/domain/GameResult.java
@@ -0,0 +1,45 @@
+package kutaverse.game.minigame.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@Table("game_results")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GameResult {
+    @Id
+    private Long id;
+
+    @Column("player1_id")
+    private String player1Id;
+
+    @Column("player2_id")
+    private String player2Id;
+
+    @Column("player1_score")
+    private int player1Score;
+
+    @Column("player2_score")
+    private int player2Score;
+
+    @Column("winner_id")
+    private String winnerId;
+
+    @CreatedDate
+    @Column("created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column("updated_at")
+    private LocalDateTime updatedAt;
+}
