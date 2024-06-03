1ccf5139fec3aa6f4e3c701b0837cbf304fc8097
kamothi <rhkddlf7911@naver.com>
2024-06-03 22:13:16 +0900

    Feat: findByUserId 메서드 추가

--- src/main/java/kutaverse/game/minigame/repository/MiniGameReactiveRepository.java
+++ src/main/java/kutaverse/game/minigame/repository/MiniGameReactiveRepository.java
@@ -3,7 +3,9 @@ package kutaverse.game.minigame.repository;
 import kutaverse.game.minigame.domain.GameResult;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 public interface MiniGameReactiveRepository extends ReactiveCrudRepository<GameResult,Long> {
 
+    Mono<GameResult> findByUserId(String userId);
 }
