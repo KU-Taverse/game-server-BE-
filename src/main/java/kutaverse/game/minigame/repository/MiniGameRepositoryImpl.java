c96908c1cc3b8a020fefb36bc248105c40c1bd05
kamothi <rhkddlf7911@naver.com>
2024-06-03 22:14:01 +0900

    Feat: repository 구현체 구현

---
+++ src/main/java/kutaverse/game/minigame/repository/MiniGameRepositoryImpl.java
@@ -0,0 +1,37 @@
+package kutaverse.game.minigame.repository;
+
+import kutaverse.game.minigame.domain.GameResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
+
+@Repository
+@RequiredArgsConstructor
+public class MiniGameRepositoryImpl implements MiniGameRepository{
+    private final MiniGameReactiveRepository mgrRepository;
+
+    @Override
+    public Mono<GameResult> findByRoomId(Long id) {
+        return mgrRepository.findById(id);
+    }
+
+    @Override
+    public Mono<GameResult> save(GameResult gameResult) {
+        return mgrRepository.save(gameResult);
+    }
+
+    @Override
+    public Mono<GameResult> update(GameResult gameResult) {
+        return mgrRepository.save(gameResult);
+    }
+
+    @Override
+    public Mono<GameResult> findByUserId(String userId) {
+        return mgrRepository.findByUserId(userId);
+    }
+
+    @Override
+    public Mono<Void> delete(Long id) {
+        return mgrRepository.deleteById(id);
+    }
+}
