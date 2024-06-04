da96896e8dcf9541fd0ec1a7154bf2b6dd4ee84f
kamothi <rhkddlf7911@naver.com>
2024-06-04 19:54:26 +0900

    feat: 실행 어플리케이션 r2dbc 어노테이션 추가

--- src/main/java/kutaverse/game/DemoApplication.java
+++ src/main/java/kutaverse/game/DemoApplication.java
@@ -2,10 +2,12 @@ package kutaverse.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableR2dbcRepositories
 public class DemoApplication {
 
 	public static void main(String[] args) {
