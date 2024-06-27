b99395202ac66fa7643514f9daff2e7deeecd2dd
david-parkk <ji0513ji@naver.com>
2024-06-27 13:26:25 +0900

    refactor

--- src/main/java/kutaverse/game/map/repository/UserRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-package kutaverse.game.map.repository;
-
-import kutaverse.game.map.domain.User;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface UserRepositoryImpl extends  UserRepository {
-}
