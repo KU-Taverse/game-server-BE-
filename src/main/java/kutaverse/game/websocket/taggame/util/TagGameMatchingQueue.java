a8ab6f927631de978e99403e26da2be6e280279b
david-parkk <ji0513ji@naver.com>
2024-09-06 00:59:23 +0900

    feat: util class

---
+++ src/main/java/kutaverse/game/websocket/taggame/util/TagGameMatchingQueue.java
@@ -0,0 +1,90 @@
+package kutaverse.game.websocket.taggame.util;
+
+import kutaverse.game.websocket.minigame.GameRoom;
+import kutaverse.game.websocket.minigame.GameRoomManager;
+import kutaverse.game.websocket.minigame.MatchingQueue;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Mono;
+
+import java.util.AbstractMap;
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+@Slf4j
+public class TagGameMatchingQueue {
+    private static final ArrayDeque<Map.Entry<String, WebSocketSession>> queueing = new ArrayDeque<>();
+    private static final Map<String, WebSocketSession> sessionMap = new ConcurrentHashMap<>();
+
+    public static void addPlayer(String userId, WebSocketSession webSocketSession){
+        if(!sessionMap.containsKey(userId)) {
+            queueing.offer(new AbstractMap.SimpleEntry<>(userId,webSocketSession));
+            sessionMap.put(userId,webSocketSession);
+            webSocketSession.send(Mono.just(webSocketSession.textMessage("매칭 대기 중 입니다."))).subscribe();
+        }
+
+    }
+
+    private static Map.Entry<String, WebSocketSession> getPlayer(){
+        return queueing.poll();
+    }
+
+    private String createGameRoom(List<Map.Entry<String, WebSocketSession>> players) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, WebSocketSession> player : players) {
+            stringBuilder.append(player.getKey());
+        }
+        String roomId = stringBuilder.toString();
+        TagGameRoom tagGameRoom = new TagGameRoom(roomId,players);
+        TagGameRoomManager.addGameRoom(tagGameRoom);
+        return roomId;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void matchPlayers(){
+        while(queueing.size() >= 4){
+            Map.Entry<String, WebSocketSession> player1 = TagGameMatchingQueue.getPlayer();
+            Map.Entry<String, WebSocketSession> player2 = TagGameMatchingQueue.getPlayer();
+            Map.Entry<String, WebSocketSession> player3 = TagGameMatchingQueue.getPlayer();
+            Map.Entry<String, WebSocketSession> player4 = TagGameMatchingQueue.getPlayer();
+
+            // 세션 값 확인
+            if(player1.getValue().isOpen() && player2.getValue().isOpen()
+            && player3.getValue().isOpen() && player4.getValue().isOpen()){
+                List<Map.Entry<String, WebSocketSession>> players = List.of(player1, player2, player3, player4);
+                String gameRoom = createGameRoom(players);
+                //플레이어들에게 gamerood 공지
+                for (Map.Entry<String, WebSocketSession> player : players) {
+                    WebSocketSession webSocketSession = player.getValue();
+                    WebSocketMessage webSocketMessage = webSocketSession.textMessage(gameRoom);
+                    webSocketSession.send(Mono.just(webSocketMessage)).subscribe();
+                }
+
+
+
+            }
+            else{
+                if(player1.getValue().isOpen()){
+                    MatchingQueue.requeue(player1);
+                }
+                if(player2.getValue().isOpen()){
+                    MatchingQueue.requeue(player2);
+                }
+                if(player3.getValue().isOpen()){
+                    MatchingQueue.requeue(player3);
+                }
+                if(player4.getValue().isOpen()){
+                    MatchingQueue.requeue(player4);
+                }
+            }
+
+        }
+    }
+}
---
+++ src/main/java/kutaverse/game/websocket/taggame/util/TagGameRoom.java
@@ -0,0 +1,20 @@
+package kutaverse.game.websocket.taggame.util;
+
+import kutaverse.game.taggame.service.TagGameService;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketSession;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+public class TagGameRoom {
+    private final String roomId;
+    private final List<Map.Entry<String, WebSocketSession>> players;
+    public TagGameRoom(String roomId, List<Map.Entry<String, WebSocketSession>> players) {
+        this.roomId = roomId;
+        this.players = players;
+    }
+
+}
