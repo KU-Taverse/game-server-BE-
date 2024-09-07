0132db28aca787c4887e2ec9383ee14ed6f75d6c
david-parkk <ji0513ji@naver.com>
2024-09-07 20:27:30 +0900

    feat: tagGameMatchResponse

---
+++ src/main/java/kutaverse/game/websocket/taggame/dto/TagGameMatchResponse.java
@@ -0,0 +1,31 @@
+package kutaverse.game.websocket.taggame.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.springframework.web.reactive.socket.WebSocketSession;
+
+import java.util.Map;
+
+@AllArgsConstructor
+@Builder
+public class TagGameMatchResponse {
+
+    private String roomId;
+
+    private String taggerId;
+
+    public static TagGameMatchResponse toDto(String roomId, Map.Entry<String, WebSocketSession> tagger) {
+        return TagGameMatchResponse.builder()
+                .roomId(roomId)
+                .taggerId(tagger.getKey())
+                .build();
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "roomId='" + roomId + '\'' +
+                ", taggerId='" + taggerId + '\'' +
+                '}';
+    }
+}
