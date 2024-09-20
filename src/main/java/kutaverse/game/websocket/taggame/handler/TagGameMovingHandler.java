ae66fcb3ac9336bf7fab3af0e5c035b1164d2d6d
david-parkk <ji0513ji@naver.com>
2024-09-20 14:42:12 +0900

    refactor: sendMessage

--- src/main/java/kutaverse/game/websocket/taggame/handler/TagGameMovingHandler.java
+++ src/main/java/kutaverse/game/websocket/taggame/handler/TagGameMovingHandler.java
@@ -3,6 +3,7 @@ package kutaverse.game.websocket.taggame.handler;
 import kutaverse.game.taggame.domain.TagGameUser;
 import kutaverse.game.taggame.repository.TagGameUserRepository;
 import kutaverse.game.websocket.taggame.dto.request.TagGameMatchRequest;
+import kutaverse.game.websocket.taggame.dto.request.TagGameRequest;
 import kutaverse.game.websocket.taggame.dto.request.TagGameUserRequest;
 import kutaverse.game.websocket.taggame.util.TagGameMatchingQueue;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +18,8 @@ public class TagGameMovingHandler implements CustomHandler{
 
     @Override
     public void handler(Object object, WebSocketSession session) {
-        TagGameUserRequest tagGameUserRequest =(TagGameUserRequest) object;
+        TagGameRequest tagGameRequest =(TagGameRequest) object;
+        TagGameUserRequest tagGameUserRequest = (TagGameUserRequest) tagGameRequest.getRequest();
         TagGameUser tagGameUser = TagGameUserRequest.toEntity(tagGameUserRequest);
         tagGameUserRepository.add(tagGameUser).subscribe();
     }
--- src/main/java/kutaverse/game/websocket/taggame/util/TagGameRunningScheduler.java
+++ src/main/java/kutaverse/game/websocket/taggame/util/TagGameRunningScheduler.java
@@ -1,31 +1,42 @@
 package kutaverse.game.websocket.taggame.util;
 
+import kutaverse.game.taggame.domain.TagGameUser;
+import kutaverse.game.taggame.repository.TagGameUserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Map;
 
 @Component
+@RequiredArgsConstructor
 public class TagGameRunningScheduler {
 
+    private final TagGameUserRepository tagGameUserRepository;
     /**
      * 멀티게임 이동 함수
      */
-
-    @Scheduled(fixedRate = 33)
+    @Scheduled(fixedRate = 3300)
     public void sendMessageToClients() {
         TagGameRoomManager.gameRooms.values()
                 .forEach(tagGameRoom -> {
                     List<Map.Entry<String, WebSocketSession>> players = tagGameRoom.getPlayers();
-                    String message = "플레이중";
-                    tagGameRoom.getPlayers().forEach(data -> {
-                        WebSocketMessage webSocketMessage = data.getValue().textMessage(message);
-                        data.getValue().send(Mono.just(webSocketMessage)).subscribe();
-                    });
+
+                    tagGameUserRepository.getAll()
+                            .collectList()   // 전체 유저 목록을 가져옴
+                            .map(JsonUtil::userListToJson)  // JSON 변환
+                            .flatMapMany(json -> Flux.fromIterable(players)
+                                    .flatMap(player -> {
+                                        WebSocketMessage webSocketMessage = player.getValue().textMessage(json);
+                                        return player.getValue().send(Mono.just(webSocketMessage));
+                                    }))
+                            .then() // 모든 작업이 완료될 때까지 대기
+                            .subscribe();
                 });
     }
 }
