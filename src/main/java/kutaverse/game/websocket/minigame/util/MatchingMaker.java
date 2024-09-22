c46f559b8fce8c9fa9c2a753197f604768f69c8b
kamothi <rhkddlf7911@naver.com>
2024-09-22 14:41:48 +0900

    feat: 종료시 disconnect 추가

--- src/main/java/kutaverse/game/websocket/minigame/util/MatchingMaker.java
+++ src/main/java/kutaverse/game/websocket/minigame/util/MatchingMaker.java
@@ -27,6 +27,16 @@ public class MatchingMaker {
             // 세션 값 확인 필요
             if(player1.getValue().isOpen() && player2.getValue().isOpen()){
                 createGameRoom(player1, player2);
+                // 비동기적으로 세션을 닫음
+                player1.getValue().close()
+                        .doOnSuccess(aVoid -> log.info("Player 1 세션이 성공적으로 닫혔습니다."))
+                        .doOnError(e -> log.error("Player 1 세션 닫기 실패", e))
+                        .subscribe();
+
+                player2.getValue().close()
+                        .doOnSuccess(aVoid -> log.info("Player 2 세션이 성공적으로 닫혔습니다."))
+                        .doOnError(e -> log.error("Player 2 세션 닫기 실패", e))
+                        .subscribe();
             }
             else{
                 if(player1.getValue().isOpen()){
@@ -53,13 +63,6 @@ public class MatchingMaker {
 
         // 여기서 카프카 로직을 추가해서 넘겨주고 세션을 close 해야함
         gameRoom.broadcastMessage(roomService.assignRoom(player1.getKey(),player2.getKey(),roomId));
-
-        try {
-            player1.getValue().close();
-            player2.getValue().close();
-        } catch (Exception e) {
-            log.error("WebSocket 세션 닫기 실패", e);
-        }
 
     }
 }
