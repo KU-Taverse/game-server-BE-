1ec2f560f0a11f18d721bcd914a24fe029313b73
kamothi <rhkddlf7911@naver.com>
2024-09-22 14:18:02 +0900

    feat: kafka message 전송 로직 추가

--- src/main/java/kutaverse/game/websocket/minigame/RoomService.java
+++ src/main/java/kutaverse/game/websocket/minigame/RoomService.java
@@ -1,23 +1,46 @@
 package kutaverse.game.websocket.minigame;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class RoomService {
 
-    private RoundRobinAllocator roundRobinAllocator;
+    private final RoundRobinAllocator roundRobinAllocator;
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final ObjectMapper objectMapper;
 
-    public RoomService() {
-        List<String> roomServers = Arrays.asList("ws://localhost:9001", "ws://localhost:9002", "ws://localhost:9003");
-        this.roundRobinAllocator = new RoundRobinAllocator(roomServers);
+    public RoomService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.roundRobinAllocator = new RoundRobinAllocator(Arrays.asList(
+                "ws://localhost:9001/game-service/game",
+                "ws://localhost:9002/game-service/game",
+                "ws://localhost:9003/game-service/game"
+        ));
+        this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = new ObjectMapper();
     }
 
-    public String assignRoom(String userId) {
+    public String assignRoom(String player1, String player2, String roomId) {
         String assignedRoomServer = roundRobinAllocator.getRoomServer();
         System.out.println("할당된 서버의 주소: " + assignedRoomServer);
+
+        // 서버 주소에서 토픽 추출
+        String topic = assignedRoomServer.split(":")[2].split("/")[0]; // "ws://localhost:9001/game-service/game"에서 "9001" 추출
+
+        // 메시지 객체 생성
+        RoomAssignmentMessage message = new RoomAssignmentMessage(player1, player2, roomId);
+
+        try {
+            String jsonMessage = objectMapper.writeValueAsString(message);
+            kafkaTemplate.send(topic, jsonMessage);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
         return assignedRoomServer;
     }
 }
--- src/main/java/kutaverse/game/websocket/minigame/util/MatchingMaker.java
+++ src/main/java/kutaverse/game/websocket/minigame/util/MatchingMaker.java
@@ -1,6 +1,7 @@
 package kutaverse.game.websocket.minigame.util;
 
 import kutaverse.game.minigame.service.MiniGameService;
+import kutaverse.game.websocket.minigame.RoomService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,6 +16,7 @@ import java.util.Map;
 public class MatchingMaker {
 
     private final MiniGameService miniGameService;
+    private final RoomService roomService;
 
     @Scheduled(fixedRate = 1000)
     public void matchPlayers(){
@@ -42,11 +44,22 @@ public class MatchingMaker {
         String roomId = player1.getKey() + "-" + player2.getKey();
         GameRoom gameRoom = new GameRoom(roomId,miniGameService);
 
-        gameRoom.addPlayer(player1.getKey(), player1.getValue());
-        gameRoom.addPlayer(player2.getKey(), player2.getValue());
+//        gameRoom.addPlayer(player1.getKey(), player1.getValue());
+//        gameRoom.addPlayer(player2.getKey(), player2.getValue());
 
-        GameRoomManager.addGameRoom(gameRoom);
+//        GameRoomManager.addGameRoom(gameRoom);
 
         gameRoom.broadcastMessage(roomId);
+
+        // 여기서 카프카 로직을 추가해서 넘겨주고 세션을 close 해야함
+        gameRoom.broadcastMessage(roomService.assignRoom(player1.getKey(),player2.getKey(),roomId));
+
+        try {
+            player1.getValue().close();
+            player2.getValue().close();
+        } catch (Exception e) {
+            log.error("WebSocket 세션 닫기 실패", e);
+        }
+
     }
 }
