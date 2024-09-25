7a60b2e286eebc8b561c83cbdc6f7cbab722f1c0
kamothi <rhkddlf7911@naver.com>
2024-09-25 18:53:49 +0900

    feat: feign 설정 및 분산 서버 상태 체크 로직 추가

--- src/main/java/kutaverse/game/DemoApplication.java
+++ src/main/java/kutaverse/game/DemoApplication.java
@@ -2,12 +2,12 @@ package kutaverse.game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableFeignClients
 public class DemoApplication {
 
 	public static void main(String[] args) {
